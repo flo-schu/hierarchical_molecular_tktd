36db8b4f647ee0db3ab49caa9a58c0cbe5f69e54
flo-schu <fluncki@protonmail.com>
2024-11-06 15:20:07 +0100

    Create a scaled version of the error model with the new likelihood function

--- sim.py
+++ sim.py
@@ -303,11 +303,11 @@ def conditional_survival_error_model_old(theta, simulation_results, observations
 def conditional_survival_error_model(theta, simulation_results, observations, masks, indices, only_prior=False, make_predictions=False):
     # indexing
     substance_idx = indices["substance_index"]
-    sigma_cint_indexed = theta["sigma_cint"][substance_idx]
-    sigma_nrf2_indexed = theta["sigma_nrf2"][substance_idx]
+    # sigma_cint_indexed = theta["sigma_cint"][substance_idx]
+    # sigma_nrf2_indexed = theta["sigma_nrf2"][substance_idx]
 
-    sigma_cint_ix_bc = jnp.broadcast_to(sigma_cint_indexed.reshape((-1, 1)), observations["cint"].shape)
-    sigma_nrf2_ix_bc = jnp.broadcast_to(sigma_nrf2_indexed.reshape((-1, 1)), observations["nrf2"].shape)
+    # sigma_cint_ix_bc = jnp.broadcast_to(sigma_cint_indexed.reshape((-1, 1)), observations["cint"].shape)
+    # sigma_nrf2_ix_bc = jnp.broadcast_to(sigma_nrf2_indexed.reshape((-1, 1)), observations["nrf2"].shape)
 
     # error model
     S = jnp.clip(simulation_results["survival"], EPS, 1 - EPS) 
@@ -326,28 +326,37 @@ def conditional_survival_error_model(theta, simulation_results, observations, ma
         obs_cint = numpyro.deterministic("cint_res", jnp.log(observations["cint"]+EPS) - jnp.log(simulation_results["cint"]+EPS))
         obs_nrf2 = numpyro.deterministic("nrf2_res", jnp.log(observations["nrf2"]+EPS) - jnp.log(simulation_results["nrf2"]+EPS))
 
+    obs_vars = ["cint", "nrf2", "survival"]
+    n = {k: masks[k].sum() for k in obs_vars}
+    N = sum(n.values())
+    weights = {k: 1/len(obs_vars) for k in obs_vars}
+    scaling_factors = {k: 1/n[k]*N*weights[k] for k in obs_vars}
+
     # calculate likelihoods
-    lik_cint = numpyro.sample("cint_obs", dist.Normal(
-            loc=0.0,  # type: ignore
-            scale=sigma_cint_ix_bc  # type: ignore
-        ).mask(masks["cint"]),
-        obs=obs_cint
-    )
+    with numpyro.handlers.scale(scale=scaling_factors["cint"]):
+        lik_cint = numpyro.sample("cint_obs", dist.Normal(
+                loc=0.0,  # type: ignore
+                scale=theta["sigma_cint"]  # type: ignore
+            ).mask(masks["cint"]),
+            obs=obs_cint
+        )
     
-    lik_nrf2 = numpyro.sample("nrf2_obs", dist.Normal(
-            loc=0.0,  # type: ignore
-            scale=sigma_nrf2_ix_bc  # type: ignore
-        ).mask(masks["nrf2"]), 
-        obs=obs_nrf2
-    )    
+    with numpyro.handlers.scale(scale=scaling_factors["nrf2"]):
+        lik_nrf2 = numpyro.sample("nrf2_obs", dist.Normal(
+                loc=0.0,  # type: ignore
+                scale=theta["sigma_nrf2"]  # type: ignore
+            ).mask(masks["nrf2"]), 
+            obs=obs_nrf2
+        )    
     
-    lik_surv = numpyro.sample(
-        "survival_obs", dist.Binomial(
-            probs=S_cond_, 
-            total_count=n_surv
-        ).mask(S_mask), 
-        obs=obs_surv
-    )
+    with numpyro.handlers.scale(scale=scaling_factors["survival"]):    
+        lik_surv = numpyro.sample(
+            "survival_obs", dist.Binomial(
+                probs=S_cond_, 
+                total_count=n_surv
+            ).mask(S_mask), 
+            obs=obs_surv
+        )
 
 
 def independent_survival_error_model(theta, simulation_results, observations, masks):
